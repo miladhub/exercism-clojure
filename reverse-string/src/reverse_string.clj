ee179816d5ce180e3d8e04b933cd27d852222133
Milad Bourhani <milad.bourhani@gmail.com>
2021-08-07 18:10:11 +0200

    first version

--- reverse-string/src/reverse_string.clj
+++ reverse-string/src/reverse_string.clj
@@ -1,5 +1,9 @@
 (ns reverse-string)
 
-(defn reverse-string [s] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn reverse-string [s]
+  (let [go (fn [s r]
+             (if (empty? s)
+               [s r]
+               (recur (subs s 1) (str (subs s 0 1) r))))
+        [_ rev] (go s "")]
+    rev))
