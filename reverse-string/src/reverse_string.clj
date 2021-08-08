fc119918d562b972c2caecd1d22b4e423828c28f
Milad Bourhani <milad.bourhani@gmail.com>
2021-08-08 15:09:02 +0200

    added alternative impl

--- reverse-string/src/reverse_string.clj
+++ reverse-string/src/reverse_string.clj
@@ -6,3 +6,14 @@
       r
       (recur (subs s 1) (str (subs s 0 1) r)))
     ))
+
+(defn reverse-string* [s]
+  (let [shift-string
+        (fn [[s r]]
+          [(subs s 1) (str (subs s 0 1) r)])
+        ]
+    (->>
+      (iterate shift-string [s ""])
+      (take (inc (count s)))
+      (last)
+      (second))))
