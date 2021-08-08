a10e9a762bc18bcf59513fada18e5096917c08e5
Milad Bourhani <milad.bourhani@gmail.com>
2021-08-08 14:37:13 +0200

    used loop

--- reverse-string/src/reverse_string.clj
+++ reverse-string/src/reverse_string.clj
@@ -1,9 +1,8 @@
 (ns reverse-string)
 
 (defn reverse-string [s]
-  (let [go (fn [s r]
-             (if (empty? s)
-               [s r]
-               (recur (subs s 1) (str (subs s 0 1) r))))
-        [_ rev] (go s "")]
-    rev))
+  (loop [s s r ""]
+    (if (empty? s)
+      r
+      (recur (subs s 1) (str (subs s 0 1) r)))
+    ))
