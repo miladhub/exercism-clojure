a186efd254d65292dd3fc06d10ba538af0483823
Milad Bourhani <milad.bourhani@gmail.com>
2021-08-13 20:47:43 +0200

    used 'into'

--- reverse-string/src/reverse_string.clj
+++ reverse-string/src/reverse_string.clj
@@ -3,17 +3,6 @@
 (defn reverse-string [s]
   (loop [s s r ""]
     (if (empty? s)
-      r
-      (recur (subs s 1) (str (subs s 0 1) r)))
+      (apply str r)
+      (recur (rest s) (into [(first s)] r)))
     ))
-
-(defn reverse-string* [s]
-  (let [shift-string
-        (fn [[s r]]
-          [(subs s 1) (str (subs s 0 1) r)])
-        ]
-    (->>
-      (iterate shift-string [s ""])
-      (take (inc (count s)))
-      (last)
-      (second))))
