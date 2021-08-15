878157fe06d44b1554e46c229c6d1ad16ca173ac
Milad Bourhani <milad.bourhani@gmail.com>
2021-08-15 21:21:56 +0200

    used into

--- reverse-string/src/reverse_string.clj
+++ reverse-string/src/reverse_string.clj
@@ -1,8 +1,4 @@
 (ns reverse-string)
 
 (defn reverse-string [s]
-  (loop [s s r ""]
-    (if (empty? s)
-      (apply str r)
-      (recur (rest s) (into [(first s)] r)))
-    ))
+  (clojure.string/join (into () s)))
