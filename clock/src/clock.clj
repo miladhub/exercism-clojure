0b19361f18ab19dd2da6480d52e361673ba8ce0d
Milad Bourhani <milad.bourhani@gmail.com>
2021-08-17 14:38:36 +0200

    first working impl

--- clock/src/clock.clj
+++ clock/src/clock.clj
@@ -1,13 +1,22 @@
 (ns clock)
 
-(defn clock->string [clock] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn clock->string [clock]
+  (str
+    (format "%02d" (clock :hours))
+    ":"
+    (format "%02d" (clock :minutes))))
 
-(defn clock [hours minutes] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn clock [hours minutes]
+  (let [tot (+ (* hours 60) minutes)
+        m (mod tot 60)
+        h' (quot tot 60)
+        h (if (and (neg? tot) (pos? m))
+            (mod (dec h') 24)
+            (mod h' 24)
+            )]
+    {:hours   h
+     :minutes m
+     }))
 
-(defn add-time [clock time] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn add-time [{h :hours m :minutes} time]
+  (clock h (+ m time)))
