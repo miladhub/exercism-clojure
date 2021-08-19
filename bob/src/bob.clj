59c9ba7bbe42f7fcad271ea63966616c25631027
Milad Bourhani <milad.bourhani@gmail.com>
2021-08-19 14:19:50 +0200

    first impl

--- bob/src/bob.clj
+++ bob/src/bob.clj
@@ -1,5 +1,18 @@
-(ns bob)
+(ns bob
+  (:require [clojure.string :as str]))
 
-(defn response-for [s] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn response-for [s]
+  (let [chars (str/join (re-seq #"[a-zA-Z]" s))
+        all-caps (and
+                   (= chars (str/upper-case chars))
+                   (not-empty chars))
+        question (str/ends-with? (str/trim s) "?")
+        yelled-question (and all-caps question)
+        not-saying-anything (empty? (re-seq #"\w" s))]
+    (cond
+      yelled-question "Calm down, I know what I'm doing!"
+      question "Sure."
+      all-caps "Whoa, chill out!"
+      not-saying-anything "Fine. Be that way!"
+      :else "Whatever."
+      )))
