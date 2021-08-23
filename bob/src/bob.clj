9ae3ea1ac971c21c616742c12f745997ca5a9cb7
Milad Bourhani <milad.bourhani@gmail.com>
2021-08-23 21:31:02 +0200

    improved

--- bob/src/bob.clj
+++ bob/src/bob.clj
@@ -4,15 +4,14 @@
 (defn response-for [s]
   (let [chars (str/join (re-seq #"[a-zA-Z]" s))
         all-caps (and
-                   (= chars (str/upper-case chars))
+                   (every? #(Character/isUpperCase ^Character %) chars)
                    (not-empty chars))
         question (str/ends-with? (str/trim s) "?")
         yelled-question (and all-caps question)
-        not-saying-anything (empty? (re-seq #"\w" s))]
+        not-saying-anything (empty? (re-seq #"[^\s]" s))]
     (cond
       yelled-question "Calm down, I know what I'm doing!"
       question "Sure."
       all-caps "Whoa, chill out!"
       not-saying-anything "Fine. Be that way!"
-      :else "Whatever."
-      )))
+      :else "Whatever.")))
