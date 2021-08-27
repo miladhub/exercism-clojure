c9a398045763dc6e31c469af0037cc2763c214b3
Milad Bourhani <milad.bourhani@gmail.com>
2021-08-27 21:40:47 +0200

    first impl

--- series/src/series.clj
+++ series/src/series.clj
@@ -1,5 +1,11 @@
 (ns series)
 
-(defn slices [string length] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn slices [string length]
+  (if (<= length 0)
+    [""]
+    (loop [string string matches []]
+      (if (< (count string) length)
+        matches
+        (recur
+          (subs string 1)
+          (conj matches (subs string 0 length)))))))
